60559cd106c1979201f785b587a4a64d7f18ef7b
yaspa69 <yaspapa69@gmail.com>
2026-09-03 08:52:46 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE='ato-nanen-v11';const A=['./','./index.html','./manifest.webmanifest'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
